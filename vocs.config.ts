978bf30e7a5e7bf99b1293f7cddfefac8ed76af2
Ryuz <ryuzer@proton.me>
2025-10-21 16:44:34 +0200

    feat(docs): add sidebar skeleton

--- vocs.config.ts
+++ vocs.config.ts
@@ -3,10 +3,12 @@ import { defineConfig } from "vocs";
 export default defineConfig({
   title: "Oxide",
   basePath: "/oxide",
+  logoUrl: "/logo-tiny.svg",
+  iconUrl: "/logo-tiny.svg",
   description:
     "Oxide.js is a Svelte and TypeScript web framework for building full-stack applications. Use Svelte to build rich user interfaces, and Oxide will provide building blocks for the server side of your application.",
   editLink: {
-    pattern: "https://github.com/oxidejs/oxide/edit/main/docs/:path",
+    pattern: "https://github.com/oxidejs/oxide/edit/main/:path",
     text: "Suggest changes to this page",
   },
   socials: [
@@ -26,7 +28,34 @@ export default defineConfig({
   sidebar: [
     {
       text: "Getting Started",
-      link: "/docs",
+      items: [
+        { text: "Start here", link: "/docs" },
+        { text: "Project Structure", link: "/docs/project-structure" },
+        { text: "Routing", link: "/docs/routing" },
+        { text: "Navigation", link: "/docs/navigation" },
+        { text: "Data Fetching", link: "/docs/data-loading" },
+        { text: "Styling", link: "/docs/styling" },
+        { text: "API Routes", link: "/docs/api-routes" },
+      ],
+    },
+    {
+      text: "Extensions",
+      items: [{ text: "ORPC", link: "/docs/orpc" }],
+    },
+    {
+      text: "Recipes",
+      items: [
+        { text: "Drizzle ORM", link: "/docs/drizzle" },
+        { text: "Better Auth", link: "/docs/better-auth" },
+        { text: "AI Streaming", link: "/docs/ai-streaming" },
+      ],
+    },
+    {
+      text: "Reference",
+      items: [
+        { text: "@oxidejs/framework", link: "/docs/oxide-framework" },
+        { text: "@oxidejs/orpc", link: "/docs/oxide-orpc" },
+      ],
     },
   ],
   topNav: [
