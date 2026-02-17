5cdbd0cd11cecfd6db441514651e937167340e23
Ryuz <ryuzer@proton.me>
2026-02-17 11:50:50 +0100

    fix(playground): adjust playwright config to run on vite dev

--- apps/playground/playwright.config.ts
+++ apps/playground/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:3000",
+    baseURL: "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -35,6 +35,8 @@ export default defineConfig({
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"], channel: "chromium" },
+      // Local development on Linux:
+      // use: { ...devices["Desktop Chrome"], channel: "chromium", launchOptions: { executablePath: "/usr/bin/chromium" } },
     },
 
     // {
